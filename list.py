9c61bb7c11bb0651dc766dee7fe08e780fe31df5
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-11 11:52:25 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    my_list = []
+    N = int(input())
+    for _ in range(N):
+        command = input().split()
+        cmd = command[0]
+
+        if cmd == 'insert':
+            i, e = int(command[1]), int(command[2])
+            my_list.insert(i, e)
+        elif cmd == 'print':
+            print(my_list)
+        elif cmd == 'remove':
+            e = int(command[1])
+            my_list.remove(e)
+        elif cmd == 'append':
+            e = int(command[1])
+            my_list.append(e)
+        elif cmd == 'sort':
+            my_list.sort()
+        elif cmd == 'pop':
+            my_list.pop()
+        elif cmd == 'reverse':
+            my_list.reverse()
